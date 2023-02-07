6cfc3250e52e5f61c341d4b7f8d30689b63bf13f
smuthu3 <selvaraju.muthusamy@walmart.com>
2023-02-06 17:18:43 -0800

    inial commit

---
+++ src/test/java/com/sel/example/examples/ExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sel.example.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
